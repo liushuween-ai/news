0a4a6e9c2b801ac4c9585f0fc62987d7a7a3b14a
liushuween-ai <2518164536@qq.com>
2019-10-25 19:52:55 +0800

    login页面用户名框和密码框

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -6,15 +6,37 @@
       <div class="logo">
           <span class="iconfont iconnew"></span>
       </div>
-      <div class="UserName"></div>
-      <div class="password"></div>
+      <div class="UserName">
+          <authInput type='text' placeholder='请输入手机号' rule='^\d{4,16}$' err_message="请输入正确的手机号"  @input="setUserName"></authInput>
+      </div>
+      <div class="password">
+          <authInput type='password' placeholder='请输入密码' rule='^\d{3,16}$' err_message="请输入正确的密码"  @input="setPassword"></authInput>
+      </div>
       <div class="loginBtn"></div>
   </div>
 </template>
 
 <script>
-export default {
+import authInput from '../components/authInput.vue';
 
+export default {
+    data(){
+        return {
+            UserName:'',
+            password:''
+        }
+    },
+    components:{
+        authInput:authInput
+    },
+    methods:{
+        setUserName(name){
+            this.UserName=name
+        },
+        setPassword(name){
+            this.password=name
+        }
+    }
 }
 </script>
 
