bc8773f4e6b3bc4c172f2f53139859f3a5909da0
liushuween-ai <2518164536@qq.com>
2019-10-25 17:59:28 +0800

    login页面·添加元素

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="login">
-      123
       <div class="loginIcon">
           <span class="iconfont iconicon-test"></span>
       </div>
+      <div class="logo">
+          <span class="iconfont iconnew"></span>
+      </div>
+      <div class="UserName"></div>
+      <div class="password"></div>
+      <div class="loginBtn"></div>
   </div>
 </template>
 
@@ -13,6 +18,30 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+    .login{
+        box-sizing: border-box;
+        width: 100%;
+        padding: 0 6.667vw;
+        .loginIcon{
+            padding-top: 20px;
+            width: 100%;  
+            .iconicon-test{
+                width: 27px;
+                height: 27px;
+                font-size: 6.667vw;
+                color: #333333;
+            }
+        }
+        .logo{
+            display: flex;
+            justify-content: center;
+            .iconnew{
+                width: 126px;
+                height: 126px;
+                font-size: 126px;
+                color: #d81e06;
+            }
+        }
+    }
 </style>
