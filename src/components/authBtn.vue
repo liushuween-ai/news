cd8613bd8201b5e3d96bf7e510b0b7dff931fcce
liushuween-ai <2518164536@qq.com>
2019-10-25 20:16:31 +0800

    搭建按钮组件

---
+++ src/components/authBtn.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="btn" @click='send'>{{text}}</div>
+</template>
+
+<script>
+export default {
+    props:['text'],
+    methods:{
+        send(){
+            this.$emit('send')
+        }
+    }
+}
+</script>
+
+<style>
+.btn{
+    height: 13.333vw;
+    line-height: 13.333vw;
+    text-align: center;
+    color: #fff;
+    background: #cc3300;
+    font-size: 5.556vw;
+    border-radius: 6.667vw;
+}
+</style>
