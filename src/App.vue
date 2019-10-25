be4abbbe8c993d2257a897b6d4a1f07630a17fe3
liushuween-ai <2518164536@qq.com>
2019-10-25 14:47:38 +0800

    初始化1

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+improt.url("http://at.alicdn.com/t/font_1426139_h6vn3jbl5q.css");
 export default {
-  name: 'App'
+  name: 'App',
 }
 </script>
 
@@ -19,5 +19,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+*{
+  margin: 0;
+  padding: 0;
+}
+a{
+  
+  color: inherit;
+}
+a:hover{
+  text-decoration: none;
+}
+ul li ol {
+  list-style: none;
 }
 </style>
---
+++ src/pages/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">
+      123
+      <div class="loginIcon">
+          <span class="iconfont iconicon-test"></span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
