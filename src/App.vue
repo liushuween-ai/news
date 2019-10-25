e6941861c53589359d23cce1cc9735dd7fd15f3e
liushuween-ai <2518164536@qq.com>
2019-10-25 17:56:04 +0800

    引入图标库

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,21 @@
 </template>
 
 <script>
-improt.url("http://at.alicdn.com/t/font_1426139_h6vn3jbl5q.css");
 export default {
   name: 'App',
 }
 </script>
 
 <style>
-#app {
+@import url(http://at.alicdn.com/t/font_1426139_h6vn3jbl5q.css);
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 *{
   margin: 0;
   padding: 0;
