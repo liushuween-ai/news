875279c1d2f44d120de9525ae50c72f1c0425733
liushuween-ai <2518164536@qq.com>
2019-10-26 08:26:27 +0800

    引用注册vant组件和引入axios组件把axios组件挂载在vue的原型对象上

--- src/main.js
+++ src/main.js
@@ -3,9 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// import Vant from 'vant'
 
-Vue.config.productionTip = false
+//引入组件库
+import Vant from 'vant';
+//注册组件库
+Vue.use(Vant);
+//引入组件样式文件
+import 'vant/lib/index.css';
 
+import axios from 'axios';
+Vue.prototype.$axios=axios;
+axios.defaults.baseURL = "http://localhost:3000";
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
