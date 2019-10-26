933eb5631e00e08ba4ae9f9075c316e590f42570
liushuween-ai <2518164536@qq.com>
2019-10-26 08:31:20 +0800

    给注册页面注册路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@/pages/Login.vue'
+import Register from '@/pages/Register'
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +16,11 @@ export default new Router({
       path:'/login',
       name:'LoginPage',
       component:Login
+    },
+    {
+      path:'/register',
+      name: 'registerPage',
+      component: Register
     }
   ]
 })
