139678ad0ab968fd1b364ad0639d9954b6441e0e
liushuween-ai <2518164536@qq.com>
2019-10-26 17:56:47 +0800

    创建自己列表组件

---
+++ src/components/authList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="list" @click="redirect">
+      <div class="title">{{title}}</div>
+      <div class="particulars">{{particulars}}
+          <span class="iconfont iconjiantou1"></span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['title','particulars'],
+    methods:{
+       redirect(){
+           this.$emit('redirect',this.title)
+       } 
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    .list{
+        display: flex;
+        justify-content: space-between;
+        height: 50px;
+        line-height: 50px;
+        margin-left: 20px;
+        border-bottom: 1px solid #eee;
+        .particulars{
+            color: #999;
+            font-size: 14px;
+        }
+    }
+</style>
