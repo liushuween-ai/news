6055586b179da6a92c63b008a813c48a6ff07346
liushuween-ai <2518164536@qq.com>
2019-10-26 11:44:30 +0800

    设置axios基准路径和拦截响应

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 // import Vant from 'vant'
+// 引入axios组件
+import axios from 'axios';
 
 //引入组件库
 import Vant from 'vant';
@@ -12,9 +14,23 @@ Vue.use(Vant);
 //引入组件样式文件
 import 'vant/lib/index.css';
 
-import axios from 'axios';
+
 Vue.prototype.$axios=axios;
-axios.defaults.baseURL = "http://localhost:3000";
+// 设置axios基准路径
+axios.defaults.baseURL = "http://127.0.0.1:3000";
+// axios.defaults.baseURL = "http://111.230.181.206:3000";
+// axios.defaults.baseURL = "http://localhost:3000";
+
+// 拦截响应
+// 因为toast插件只能在组件里面用
+import {Toast} from 'vant'
+axios.interceptors.response.use(res=>{
+  const {message,statusCode}=res.data
+  if (message && statusCode == 401 ||res.data.statusCode == 400) {
+    Toast.fail(message)
+  }
+  return(res)
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
