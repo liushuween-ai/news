43a7c1a519ba9b8bf1c6b46fee09567aff3efedf
liushuween-ai <2518164536@qq.com>
2019-10-27 16:14:48 +0800

    创建头部中间组件

---
+++ src/components/headerMiddle.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="vessel">
+      <span class="iconfont iconjiantou2"></span>
+      <div class="text">{{title}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['title']
+}
+</script>
+
+<style lang="less" scoped>
+.vessel{
+    height: 13.889vw;
+    line-height: 13.889vw;
+    .iconfont{
+        padding-left: 5.556vw;
+    }
+    .text{
+        position: absolute;
+        left: 41.111vw;
+        top: 0;
+        font-weight: 600
+    }
+}
+</style>
