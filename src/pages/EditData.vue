642fcf99ba55a5d2314efc33d1af98a6e9f4b6bd
liushuween-ai <2518164536@qq.com>
2019-10-28 19:50:31 +0800

    给编辑资料页面添加修改功能

--- src/pages/EditData.vue
+++ src/pages/EditData.vue
@@ -2,13 +2,23 @@
   <div>
       <headerMiddle title="编辑资料"></headerMiddle>
       <div class="headPortraits">
-         <img v-if="data.head_img" :src="data.head_img" alt="">
-         <img v-else src="@/assets/logo.png" alt="">          
+         <img  :src="data.head_img" alt="">
+         <van-uploader :after-read="afterRead" class="upFile" />   
       </div>
-      <authList title="昵称" :particulars='data.nickname'></authList>
-      <authList title="密码" particulars='******'></authList>
-      <authList v-if="data.gender===1" title="性别" particulars='男'></authList>
-      <authList v-else title="性别" particulars='女'></authList>      
+      <authList title="昵称" :particulars='data.nickname' @redirect="isShowNickname=true"></authList>
+      <authList title="密码" particulars='******' @redirect="isShowPassword=true"></authList>
+      <authList title="性别" :particulars='data.gender' @redirect="isShowGender=true"></authList>
+      <!-- <authList v-else title="性别" particulars='女' ></authList>   -->
+      <!-- 这里是放组件的组件       -->
+      <van-dialog v-model="isShowNickname" title="编辑昵称" show-cancel-button="" @confirm="editNackname">
+          <van-field type="text" placeholder="输入昵称" v-model="nickname" ></van-field>
+      </van-dialog>
+      <van-dialog v-model="isShowPassword" title="编辑密码" show-cancel-button="" @confirm="editPassword">
+          <van-field type="password" placeholder="输入密码" v-model="password" ></van-field>
+      </van-dialog>
+      <van-action-sheet v-model="isShowGender" :actions="gender" @select="editGender" cancel-text="取消"></van-action-sheet>
+      
+  
   </div>
 </template>
 
@@ -18,36 +28,106 @@ import authList from '../components/authList'
 export default {
     data(){
         return({
-            data:[]
+            data:[],
+            isShowNickname:false,
+            nickname:'',
+            isShowPassword:false,
+            password:'',
+            newData:{},
+            isShowGender:false,
+            gender:[{name:'男'},{name:'女'}],
+            editGenderNub:'',
+            
         })
     },
     components:{
         headerMiddle,
         authList
     },
-    mounted(){
-        this.$axios({
+    methods:{
+         afterRead(uploaderFile) {
+            // 此时可以自行将文件上传至服务器
+            console.log(uploaderFile.file);
+            const data =new FormData();
+            data.append('file',uploaderFile.file);
+            this.$axios({
+                url:'/upload',
+                method:'post',
+                data:data
+            }).then(res=>{
+                // console.log(res.data.data);
+                // console.log(this.$axios.defaults.baseURL + res.data.data.url);  
+                this.editPageData({head_img:res.data.data.url})
+            })
+        },
+        getPageData(){
+             this.$axios({
             url:'http://127.0.0.1:3000/user/'+localStorage.getItem('user_id'),
             method:'get',
             headers:{
                 Authorization:localStorage.getItem('token')
             }
-        }).then(res=>{
-            console.log(res.data.data),
-            this.data=res.data.data
+            }).then(res=>{
+            // console.log(res.data.data)
+            this.data=res.data.data;
+            if(this.data.head_img) {
+                this.data.head_img=this.$axios.defaults.baseURL+ this.data.head_img;
+                // console.log(this.$axios.defaults.baseURL)
+            }else{
+                this.data.head_img='/static/imgs/a.jpg'
+            }
+            this.data.gender= this.data.gender===1? '男':'女';
         })
+        },
+        editPageData(newData){
+            this.$axios({
+                url:'/user_update/'+localStorage.getItem('user_id'),
+                method:'POST',
+                headers:{
+                    Authorization:localStorage.getItem('token')
+                },
+                data:newData,
+            }).then(res=>{
+                this.getPageData();
+            })
+        },
+        editNackname(){
+           this.editPageData({nickname:this.nickname})
+        },
+        editPassword(){
+            this.editPageData({password:this.password})
+        },
+        editGender(item){
+            // this.gender=event.detail;
+            // console.log(item)
+            this.editGenderNub = item.name==='女' ? 0 : 1;
+            // console.log(this.editGenderNub)
+            this.editPageData({gender:this.editGenderNub});
+            this.isShowGender=false;
+        }
+    },
+    mounted(){
+       this.getPageData();
     }
 }
 </script>
 
 <style lang="less" scoped>
     .headPortraits{
+        position: relative;
+        display: flex;
+        margin: 5.556vw 0;
+        justify-content: center;
         img{  
-           margin: 5.556vw 0 5.556vw 41.667vw;
             width: 19.444vw;
             height: 19.444vw;
             border-radius: 50%;
         }
-
+        .upFile{
+            position: absolute;
+            top: 0;
+            margin: 0;
+            opacity: 0;
+        }
     }
 </style>
