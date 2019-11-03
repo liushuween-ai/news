5710388fd31b60a00e4b3c402752cfdb9bc2d290
liushuween-ai <2518164536@qq.com>
2019-11-03 11:25:31 +0800

    判断本地储存里面有没有用户信息

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -43,22 +43,36 @@ export default {
   methods:{
     initTabList(){
       // 挂载完毕的钩子函数
-      this.$axios({
-      url:'/category',
-      method:'get',
-    }).then(res=>{
-      // console.log(res);
-      const {data}=res.data;
-      // 给获取的每一项数据添加一个props 用来存放文章数据
-      data.forEach(element => {
-        element.props=[],
-        element.currentPage =1,
-        element.finished=false
-      });
-      this.tabList=data;
-       // 获取完了 tab 数据, 马上使用默认 tab (头条) 获取文章列表数据
-       this.getTabProps(this.activeTab)
-    })
+      if(localStorage.getItem('tabActive')){
+        const data=JSON.parse(localStorage.getItem('tabActive'));
+         // 给获取的每一项数据添加一个props 用来存放文章数据
+          data.forEach(element => {
+            element.props=[],
+            element.currentPage =1,
+            element.finished=false
+          });
+          this.tabList=data;
+          // 获取完了 tab 数据, 马上使用默认 tab (头条) 获取文章列表数据
+          this.getTabProps(this.activeTab)
+      }else{
+        this.$axios({
+          url:'/category',
+          method:'get',
+        }).then(res=>{
+          // console.log(res);
+          const {data}=res.data;
+          // 给获取的每一项数据添加一个props 用来存放文章数据
+          data.forEach(element => {
+            element.props=[],
+            element.currentPage =1,
+            element.finished=false
+          });
+          this.tabList=data;
+          // 获取完了 tab 数据, 马上使用默认 tab (头条) 获取文章列表数据
+          this.getTabProps(this.activeTab)
+        })
+      }
+      
     },
     loadMorePost(){
       this.tabList[this.activeTab].currentPage++;
